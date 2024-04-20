801d20619fcb86f2490f817eb87bc53d49ecc706
LongLD <longld@fabbi.com.vn>
2024-04-21 05:57:27 +0700

    refactor(frontend): update ribbon

--- src/components/features/ribbon/index.tsx
+++ src/components/features/ribbon/index.tsx
@@ -11,10 +11,11 @@ function Ribbon() {
       case 'localhost':
         setEnv('LOCAL');
         break;
-      case 'dev.kyupad.xyz':
-        setEnv('DEV');
-        break;
       default:
+        if (hostname !== 'kyupad.xyz') {
+          const arr = hostname.split('.');
+          setEnv(arr[0].toUpperCase);
+        }
         break;
     }
   }, []);
@@ -23,7 +24,8 @@ function Ribbon() {
     <>
       {env && (
         <div className="fixed left-0 top-0 h-16 w-16">
-          <div className="absolute transform -rotate-45 bg-kyu-color-4 text-center text-kyu-color-10 font-bold py-1 left-[-50px] top-[20px] w-[170px] font-sans shadow">
+          <div
+            className="absolute transform -rotate-45 bg-kyu-color-4 text-center text-kyu-color-10 font-bold py-1 left-[-50px] top-[20px] w-[170px] font-sans shadow">
             {env}
           </div>
         </div>
