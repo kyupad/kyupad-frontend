00b8c8c68694a75b25c1ead9b3dd7c49c86f2fa2
truongdn <namtruongdev@gmail.com>
2024-05-04 09:45:31 +0700

    feat: update top page part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Knewave } from 'next/font/google';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
 import PrimaryButton from '@/components/common/button/primary';
+import Skeleton from '@/components/common/loading/skeleton';
 import FurtureLaunch from '@/components/features/landing/furture-launch';
 import SuccessLaunch from '@/components/features/landing/success-launch';
 import UpcomingLaunch from '@/components/features/landing/upcoming-launch';
@@ -73,7 +74,21 @@ const Home = async () => {
           </h3>
 
           <div className="py-5">
-            <Suspense fallback={null}>
+            <Suspense fallback={<>Loading...</>}>
+              <div className="w-full">
+                <div className="pb-10">
+                  <Skeleton className="h-[14px] sm:h-6 md:h-[30px] lg:h-9 w-1/3 mx-auto" />
+                </div>
+                <div className="flex bg-[#FFF9EB] border-4 lg:p-10 border-button-primary-border rounded-[12px] shadow-[19px_18px_0px_0px_rgba(42,_39,_58,_0.1)] md:w-full lg:gap-10 flex-col lg:flex-row relative">
+                  <div className="w-full lg:w-5/12 px-6 pb-6 pt-3 lg:p-0 flex gap-6 flex-col order-2 lg:order-1">
+                    <Skeleton className="h-5" />
+                    <Skeleton className="h-[180px] rounded-[24px]" />
+                  </div>
+                  <div className="lg:w-7/12 relative rounded-tl-[8px] rounded-tr-[8px] lg:rounded-[8px] overflow-hidden order-1 lg:order-2 pb-[56.25%] lg:pb-0">
+                    <Skeleton className="h-full" />
+                  </div>
+                </div>
+              </div>
               <FurtureLaunch />
             </Suspense>
           </div>
