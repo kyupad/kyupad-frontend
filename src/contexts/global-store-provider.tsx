16dbfe9681e52fb88c915937138e673b8a281927
truongdn <namtruongdev@gmail.com>
2024-05-07 16:45:05 +0700

    fix: sentry not working in prod

--- src/contexts/global-store-provider.tsx
+++ src/contexts/global-store-provider.tsx
@@ -18,8 +18,10 @@ import {
   REFRESH_TOKEN_COOKIE_CONFIG,
   REFRESH_TOKEN_STORAGE_KEY,
 } from '@/utils/constants';
+import * as Sentry from '@sentry/nextjs';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { deleteCookie, getCookie, hasCookie, setCookie } from 'cookies-next';
+import { env } from 'env.mjs';
 import jsonwebtoken from 'jsonwebtoken';
 import { useStore, type StoreApi } from 'zustand';
 import { EWalletName } from '@utils/enums/solana';
@@ -40,6 +42,36 @@ const GlobalStoreProvider = ({
   if (!storeRef.current) {
     storeRef.current = createGlobalStore;
   }
+
+  useEffect(() => {
+    Sentry.init({
+      dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+
+      replaysOnErrorSampleRate: 1.0,
+
+      // This sets the sample rate to be 10%. You may want this to be 100% while
+      // in development and sample at a lower rate in production
+      replaysSessionSampleRate: 0.1,
+
+      // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+      integrations: [
+        Sentry.replayIntegration({
+          // Additional Replay configuration goes in here, for example:
+          maskAllText: true,
+          blockAllMedia: true,
+        }),
+        Sentry.captureConsoleIntegration({
+          levels: ['error', 'debug'],
+        }),
+      ],
+    });
+  }, []);
 
   const { disconnect, connected, wallet } = useWallet();
 
