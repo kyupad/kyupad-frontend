53b1fb95fadf85cae49f000ae99afe08d617e55c
truongdn <namtruongdev@gmail.com>
2024-05-20 15:18:55 +0700

    feat: final investing

--- src/components/features/detail/view-investment/invest-more-popup.tsx
+++ src/components/features/detail/view-investment/invest-more-popup.tsx
@@ -9,6 +9,7 @@ import {
   DialogTrigger,
 } from '@/components/common/dialog';
 import { Input } from '@/components/common/input';
+import { ShowAlert } from '@/components/common/toast';
 import PrimaryButton from '@components/common/button/primary';
 
 function InvestMorePopup({
@@ -19,15 +20,62 @@ function InvestMorePopup({
 }: {
   children: ReactNode;
   amount?: number;
-  handleInvest: (numberTicket: number) => void;
+  handleInvest: (_: any, numberTicket: number) => void;
   loading?: boolean;
 }) {
   const [numberTicket, setNumberTicket] = useState<number>(1);
 
   const handleChangeNumberTicket = useCallback((e: any) => {
-    const value = e.target.value || 1;
-    setNumberTicket(value);
+    const value = e.target.value;
+
+    return setNumberTicket(value);
   }, []);
+
+  const renderError = useCallback(() => {
+    if (!numberTicket) {
+      return (
+        <div className="text-red-500 text-sm">
+          Please enter the number of tickets
+        </div>
+      );
+    }
+
+    if (numberTicket < 1) {
+      return (
+        <div className="text-red-500 text-sm">
+          The number of tickets must be greater than 0
+        </div>
+      );
+    }
+    if (amount && numberTicket > amount) {
+      return (
+        <div className="text-red-500 text-sm">
+          You can&apos;t invest more than {amount} tickets
+        </div>
+      );
+    }
+  }, [amount, numberTicket]);
+
+  const validator = (cb: Function) => {
+    if (!numberTicket) {
+      return ShowAlert.error({
+        message: 'Please enter the number of tickets',
+      });
+    }
+
+    if (numberTicket < 1) {
+      return ShowAlert.error({
+        message: 'The number of tickets must be greater than 0',
+      });
+    }
+    if (amount && numberTicket > amount) {
+      return ShowAlert.error({
+        message: `You can't invest more than ${amount} tickets`,
+      });
+    }
+
+    return cb(undefined, numberTicket);
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -48,21 +96,22 @@ function InvestMorePopup({
         </DialogHeader>
         <div className="grid gap-4">
           <Input
+            value={numberTicket}
             label=""
             type="number"
             max={amount || 1}
             min={1}
             placeholder="1"
-            defaultValue={numberTicket}
             onChange={handleChangeNumberTicket}
           />
+          {renderError()}
         </div>
         <DialogFooter className="-mt-3">
           <PrimaryButton
             disabled={loading}
             loading={loading}
             block
-            onClick={() => handleInvest(numberTicket)}
+            onClick={() => validator(handleInvest)}
           >
             Invest
           </PrimaryButton>
